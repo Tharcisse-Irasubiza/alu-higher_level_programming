6bffbb82f2d5baf98ef2f70721b3efa7a3ec2c56
Tharcisse-Irasubiza <t.irasubiza@alustudent.com>
2026-07-18 03:58:28 -0700

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Defines a square by its size."""
+
+    def __init__(self, size):
+        """Initialize a Square with a private size attribute."""
+        self.__size = size
