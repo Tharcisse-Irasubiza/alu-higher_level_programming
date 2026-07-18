153247028461c6a910415c13dc1f35047d9ab49d
Tharcisse-Irasubiza <t.irasubiza@alustudent.com>
2026-07-18 07:05:37 -0700

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Module that defines a custom list class."""
+
 
 class MyList(list):
+    """A class that inherits from the built-in list class."""
+
     def print_sorted(self):
+        """Print the list in ascending sorted order."""
         print(sorted(self))
