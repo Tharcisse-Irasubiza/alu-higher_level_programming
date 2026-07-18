b500f21d63e861917ebc8a26935d2d422d71b3ac
Tharcisse-Irasubiza <t.irasubiza@alustudent.com>
2026-07-18 07:19:51 -0700

    2-is_same_class.py

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Module that provides a function"""
+"""Module that provides a function """
+
 
 def is_same_class(obj, a_class):
-    """Return True if obj is exactl"""
-
+    """Return True if obj is exactly an instance """
     return type(obj) == a_class
-
