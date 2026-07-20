f7d8196d84c7e3fe7c0c6c6a62106e90bcfc3699
Tharcisse-Irasubiza <t.irasubiza@alustudent.com>
2026-07-20 03:27:49 -0700

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- List privileges of user_0d_1 and user_0d_2 111
+-- List privileges of user_0d_1 and user_0d_2 1111
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
