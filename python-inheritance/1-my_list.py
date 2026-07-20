eb4010bac911a836d9a52e20444119e36b11f316
Tharcisse-Irasubiza <t.irasubiza@alustudent.com>
2026-07-20 03:59:29 -0700

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Module that defines MyList."""
+"""Defines a class MyList that inherits from list."""
 
 
 class MyList(list):
-    """A class that inherits from list."""
+    """Represents a list with an additional print_sorted method."""
 
     def print_sorted(self):
-        """Print the list sorted in ascending order."""
+        """Print the list in ascending sorted order without modifying it."""
         print(sorted(self))
