d5b54a215552a4f11c648a1bade0335dbf5e35b3
Tharcisse-Irasubiza <t.irasubiza@alustudent.com>
2026-07-23 13:06:39 +0200

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,3 @@
--- Remove users if they exist
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
-
--- List privileges
+-- Lists all privileges of user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
