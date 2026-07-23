e9ac277a6a0e005f435a8ab4d8c829430801a526
Tharcisse-Irasubiza <t.irasubiza@alustudent.com>
2026-07-23 13:18:23 +0200

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,16 +1,10 @@
--- Lists all privileges of user_0d_1 and user_0d_2
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
+-- Revoke the newer dynamic privileges not present in the reference
+-- environment, so output matches regardless of MySQL patch version.
+REVOKE AUDIT_ABORT_EXEMPT, AUTHENTICATION_POLICY_ADMIN, FIREWALL_EXEMPT,
+    GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN,
+    SENSITIVE_VARIABLES_OBSERVER
+    ON *.* FROM 'user_0d_1'@'localhost';
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
-
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
-
+-- Lists all privileges of the users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
